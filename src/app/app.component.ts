555bbd4a1bc030f157ff2f5be20485363f779f12
DjalmilaChabane <djamila.chabane@lepoles.com>
2019-03-11 16:31:22 +0100

    play with app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tour-of-heroes';
-}
+  title = 'retour des héros';
+  heroes = [
+    'Superman',
+    'Wonderwoman',
+    'Prettywoman',
+  ];
+  name = '';
+  heroToAdd='';
+  addHero(){
+    // si heroToAdd = ' coucou '
+    // alors heroToAdd.trim() = 'coucou'
+    // donc si heroToAdd = '         '
+    // alors heroToAdd = ''
+      if(this.heroToAdd !== ''){ // ou if(this.heroToAdd.trim().length > 0)
+    this.heroes.push(this.heroToAdd.trim());
+    // this represente le heroes qui se trouve dans la classe courrante qui est AppComponent
+    this.heroToAdd = '';
+  }else{
+    alert('le nom du héros ne doit pas etre vide');
+  }
+  
+} 
+}// fin de classAppComponent
